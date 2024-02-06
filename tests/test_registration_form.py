d8b582816f13deb7787586a0ec156ed42cf7bef8
Nikita <nikita.volnyagin@yandex.ru>
2024-02-06 15:49:14 +0400

    GOOOO#4

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -5,29 +5,29 @@ def test_practice_from():
     registration_page = RegistrationPage()
     registration_page.open_registration_page()
 
-    registration_page.fill_first_name('Sergey')
-    registration_page.fill_last_name('Gavrilenko')
-    registration_page.fill_user_email('serezagavrilenko@gmail.com')
+    registration_page.fill_first_name('Nikita')
+    registration_page.fill_last_name('Nikitin')
+    registration_page.fill_user_email('test.test@test.com')
     registration_page.gender_selection('Male')
-    registration_page.fill_user_phone_number(9920454526)
-    registration_page.fill_date_of_birth('2003', 'June', '03')
+    registration_page.fill_user_phone_number(7999158449)
+    registration_page.fill_date_of_birth('1998', 'April', '27')
     registration_page.select_user_subject('Economics')
     registration_page.user_hobby_checkbox('Sports')
-    registration_page.user_picture('photo.jpg')
-    registration_page.user_current_adress('Artistic driveway 4, sq.1')
+    registration_page.user_picture('image.jpg')
+    registration_page.user_current_adress('Russia, Samara, 446222')
     registration_page.user_state('NCR')
     registration_page.user_city('Noida')
     registration_page.submit_the_form()
 
     registration_page.should_registered_user_with(
-        'Sergey Gavrilenko',
-        'serezagavrilenko@gmail.com',
+        'Nikita Nikitin',
+        'test.test@test.com',
         'Male',
-        '9920454526',
-        '03 June,2003',
+        '7999158449',
+        '20 April,1998',
         'Economics',
         'Sports',
-        'photo.jpg',
-        'Artistic driveway 4, sq.1',
+        'Image.jpg',
+        'Russia, Samara, 446222',
         'NCR Noida',
     )
