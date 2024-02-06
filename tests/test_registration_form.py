47e073f94d9fadf2164555e2860dda3789e12272
Nikita <nikita.volnyagin@yandex.ru>
2024-02-06 15:41:49 +0400

    GOOOO#3

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,37 +1,33 @@
-from selene import browser, have, be, command
-import os
+from pages.registration_page import RegistrationPage
 
 
-def test_registration_form_demo_qa():
-    browser.open('/automation-practice-form')
-    browser.element('#firstName').type("Nikita")
-    browser.element('#lastName').type("Nikitin")
-    browser.element('#userEmail').type('test.test@test.com')
-    browser.all('[for^=gender-radio]').element_by(have.exact_text('Male')).click()
-    browser.element('#userNumber').type('7999158449')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__month-select').click()
-    browser.element('.react-datepicker__month-select [value="3"]').click()
-    browser.element('.react-datepicker__year-select').click()
-    browser.element('.react-datepicker__year-select [value="1998"]').click()
-    browser.element('.react-datepicker__day--020').click()
-    browser.element('#subjectsInput').should(be.blank).type('arts').press_enter()
-    browser.all('[for^=hobbies-checkbox-3]').element_by(have.exact_text('Music')).click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath('img/Image.jpg'))
-    browser.element('#currentAddress').type('Russia, Samara, 446222')
-    browser.element('.css-yk16xz-control').click()
-    browser.element('#react-select-3-input').type('NCR').press_enter()
-    browser.element('#react-select-4-input').type('Noida').press_enter()
-    browser.element('#submit').perform(command.js.click)
+def test_practice_from():
+    registration_page = RegistrationPage()
+    registration_page.open_registration_page()
 
-    browser.element('.table').all('td').even.should(have.exact_texts(
-        'Nikita Nikitin',
-        'test.test@test.com',
+    registration_page.fill_first_name('Sergey')
+    registration_page.fill_last_name('Gavrilenko')
+    registration_page.fill_user_email('serezagavrilenko@gmail.com')
+    registration_page.gender_selection('Male')
+    registration_page.fill_user_phone_number(9920454526)
+    registration_page.fill_date_of_birth('2003', 'June', '03')
+    registration_page.select_user_subject('Economics')
+    registration_page.user_hobby_checkbox('Sports')
+    registration_page.user_picture('photo.jpg')
+    registration_page.user_current_adress('Artistic driveway 4, sq.1')
+    registration_page.user_state('NCR')
+    registration_page.user_city('Noida')
+    registration_page.submit_the_form()
+
+    registration_page.should_registered_user_with(
+        'Sergey Gavrilenko',
+        'serezagavrilenko@gmail.com',
         'Male',
-        '7999158449',
-        '20 April,1998',
-        'Arts',
-        'Music',
-        'Image.jpg',
-        'Russia, Samara, 446222',
-        'NCR Noida'))
+        '9920454526',
+        '03 June,2003',
+        'Economics',
+        'Sports',
+        'photo.jpg',
+        'Artistic driveway 4, sq.1',
+        'NCR Noida',
+    )
